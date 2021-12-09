e795a8df5764d8a445402047ffc2cd1f04d77e7e
Niklas Olsson <niklas@jadeva.se>
2021-12-09 22:53:09 +0100

    Day 9 - 2

--- AdventOfCode2021/Day9/Day9.cs
+++ AdventOfCode2021/Day9/Day9.cs
@@ -1,18 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 
 namespace AdventOfCode2021.Day9
 {
     public class Day9 : DayBase
     {
+        private PointComparer _pointComparer = new PointComparer();
         public Day9()
             : base(9) { }
         
         public override void Run1()
         {
             var map = GetInputAsInts();
-            var lowPoints = new List<int>();
+            var lowPoints = GetLowPoints(map);
+            var result = lowPoints.Sum(p => p.Value) + lowPoints.Count;
+            Console.WriteLine("Answer is: " + result);
+        }
+
+        private int[,] GetInputAsInts()
+        {
+            var input = GetInputAsStringList(1);
+            var ints = new int[input.Count, input[0].Length];
+            for (int i = 0; i < input.Count; i++)
+            {
+                for (int j = 0; j < input[i].Length; j++)
+                    ints[i, j] = int.Parse(input[i][j].ToString());
+            }
+
+            return ints;
+        }
+        private List<Point> GetLowPoints(int[,] map)
+        {
+            var lowPoints = new List<Point>();
             var width = map.GetLength(1);
             var height = map.GetLength(0);
             
@@ -30,27 +51,84 @@ namespace AdventOfCode2021.Day9
                     if (col + 1 < width && map[row, col + 1] <= value)
                         continue;
                     
-                    lowPoints.Add(value);
+                    lowPoints.Add(new Point(row, col, value));
                 }   
             }
-            var result = lowPoints.Sum() + lowPoints.Count;
-            Console.WriteLine("Answer is: " + result);
-        }
 
-        private int[,] GetInputAsInts()
-        {
-            var input = GetInputAsStringList(1);
-            var ints = new int[input.Count, input[0].Length];
-            for (int i = 0; i < input.Count; i++)
-            {
-                for (int j = 0; j < input[i].Length; j++)
-                    ints[i, j] = int.Parse(input[i][j].ToString());
-            }
-
-            return ints;
+            return lowPoints;
         }
         public override void Run2()
         {
+            var map = GetInputAsInts();
+            var lowPoints = GetLowPoints(map);
+
+
+            List<List<Point>> basins = new List<List<Point>>();
+            foreach (var lowPoint in lowPoints)
+            {
+                List<Point> basin = new List<Point>();
+                basin.Add(lowPoint);
+                FindAdjacent(lowPoint, map, basin);
+                basins.Add(basin);
+            }
+
+            var orderedBasins = basins.OrderByDescending(b => b.Count).ToArray();
+            Console.WriteLine("Answer is: " + (orderedBasins[0].Count * orderedBasins[1].Count * orderedBasins[2].Count));
+            
+        }
+
+        private void FindAdjacent(Point point, int[,] map, List<Point> basin)
+        {
+            var width = map.GetLength(1);
+            var height = map.GetLength(0);
+
+            if (point.Row - 1 >= 0 && map[point.Row - 1, point.Col] < 9)
+                AddNewPointToList(point.Row - 1, point.Col, map, basin);
+            if (point.Row + 1 < height && map[point.Row + 1, point.Col] < 9)
+                AddNewPointToList(point.Row + 1, point.Col, map, basin);
+            if (point.Col - 1 >= 0 && map[point.Row, point.Col - 1] < 9)
+                AddNewPointToList(point.Row, point.Col - 1, map, basin);
+            if (point.Col + 1 < width && map[point.Row, point.Col + 1] < 9)
+                AddNewPointToList(point.Row, point.Col + 1, map, basin);
+        }
+        private void AddNewPointToList(int row, int col, int[,] map, List<Point> basin)
+        {
+            var newPoint = new Point(row, col, map[row, col]);
+            if (!basin.Contains(newPoint, _pointComparer))
+            {
+                basin.Add(newPoint);
+                FindAdjacent(newPoint, map, basin);
+            }
+        }
+
+        private class Point
+        {
+            public int Row { get; }
+            public int Col { get; }
+            public int Value { get; }
+
+            public Point(int row, int col, int value)
+            {
+                Row = row;
+                Col = col;
+                Value = value;
+            }
+        }
+        
+        private class PointComparer : IEqualityComparer<Point>
+        {
+            public bool Equals(Point x, Point y)
+            {
+                if (ReferenceEquals(x, y)) return true;
+                if (ReferenceEquals(x, null)) return false;
+                if (ReferenceEquals(y, null)) return false;
+                if (x.GetType() != y.GetType()) return false;
+                return x.Row == y.Row && x.Col == y.Col;
+            }
+            public int GetHashCode(Point obj)
+            {
+                return HashCode.Combine(obj.Row, obj.Col);
+            }
         }
     }
 }
